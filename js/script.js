7f9262890625686947e3376db2514a31804b8c57
Andrii Murlykin <109280143+Murlykin@users.noreply.github.com>
2023-06-12 17:58:21 +0300

    Finish

--- js/script.js
+++ js/script.js
@@ -8,25 +8,25 @@ document.addEventListener('DOMContentLoaded', function () {
         e.preventDefault();
         let error = formValidate(form);
         
-        console.log (formValidate(form))
+    
         let formData = new FormData (form)
 
         if (error === 0) {
-            // form.classList.add('_sending');
-            // let response = await fetch('sendmail.php', {
-            //     method: 'POST',
-            //     body: formData
+            form.classList.add('_sending');
+            let response = await fetch('sendmail.php', {
+                method: 'POST',
+                body: formData
                 
-            // });
-            // if (response.ok) {
-            //     let result = await response.json();
-            //     alert(result.message);
-            //     formPreview.innerHTML = '';
-            //     form.reset();
-            // }
-            // else {
-            //     alert("Ошибка")
-            // }
+            });
+            if (response.ok) {
+                let result = await response.json();
+                alert(result.message);
+                formPreview.innerHTML = '';
+                form.reset();
+            }
+            else {
+                alert("Ошибка")
+            }
 }
          else {
             alert('Заполните обязательные поля')
@@ -35,37 +35,38 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function formValidate(form) {
         let error = 0;
-        let formReq = document.querySelectorAll('._req');
-// console.log(error)
-        for (let index = 0; index < formReq.length; index++) {
-    const input = formReq[index];
-            formRemoveError(input);
+                let formReq = document.querySelectorAll('._req');
+        // console.log(error)
+                for (let index = 0; index < formReq.length; index++) {
+            const input = formReq[index];
+                    formRemoveError(input);
 
-            if (input.classList.contains('_email')) {
-                if (emailTest(input)) {
-                    formAddError(input);
-                    error++;
+                    if (input.classList.contains('_email')) {
+                        if (emailTest(input)) {
+                            formAddError(input);
+                            error++;
+                        }
+                    }
+                    else {
+                        if (input.value === '') {
+                            formAddError(input);
+                            error++;
+                        }
+                    }
                 }
-            }
-            else {
-                if (input.value === '') {
-                    formAddError(input);
-                    error++;
-                }
-            }
-        }
    
-    function formAddError(input) {
-        input.parentElement.classList.add('_error');
-        input.classList.add('_error');
-    }
-        function formRemoveError(input) {
-        input.parentElement.classList.remove('_error');
-        input.classList.remove('_error');
-    }
- }
-    function emailTest(input) {
-        return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+            function formAddError(input) {
+                input.parentElement.classList.add('_error');
+                input.classList.add('_error');
+            }
+                function formRemoveError(input) {
+                input.parentElement.classList.remove('_error');
+                input.classList.remove('_error');
+            }
+         }
+            function emailTest(input) {
+                return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+
     }
 });
 
---
+++ sendmail.php
@@ -0,0 +1,13 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/PHPMailer.php';
+
+$mail = new PHPMailer(true);
+$mail->CharSet = 'UTF-8';
+$mail->IsHTML(true);
+
+$mail->addAddress('designer.web.studio.rgb@gmail.com');
+
